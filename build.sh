2b1b8dc35d518d8bab18cedf957126ae9e4d51af
Leonardo Araújo <ledharaujo@gmail.com>
2025-11-18 14:42:43 -0300

    feat: adiciona ci para site

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64 -t personal-site:3 .
+TAG=$1
 
+docker buildx build --platform linux/amd64 -t personal-site:$TAG .
+docker tag personal-site:$TAG ledharaujo/personal-site:$TAG
+docker push ledharaujo/personal-site:$TAG
